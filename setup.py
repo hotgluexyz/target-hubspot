c456f17b9ecbdc680148440464d145aaeaafa92a
Hassan Syyid <hassansyyid@gmail.com>
2022-01-13 09:09:50 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='target-hubspot',
-    version='1.0.2',
+    version='1.0.3',
     description='hotglue target for exporting data to Hubspot API',
     author='hotglue',
     url='https://hotglue.xyz',
